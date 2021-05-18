6616789fd6687b5975dd7fbf6b4d026f92d5b913
Andre Jaques <andrejaques1994@gmail.com>
2021-05-18 14:49:26 -0300

    configurando landing page

--- src/pages/LandingPage/landingPage.js
+++ src/pages/LandingPage/landingPage.js
@@ -14,6 +14,7 @@ function LandingPage() {
             <Menu/>
 
             <main id="mainLanding">
+                
                 <section id="principal">
 
                     <div className="container">
@@ -36,81 +37,7 @@ function LandingPage() {
 
                 </section>
             
-                <section id="print">
-
-                    <div className="container">
-
-                        <div id="textoPrint">
-                        <h2>Oferecemos um conteúdo mais elaborado e personalizado</h2>
-
-                        <p>Organizamos por dia da semana, disciplina e assunto. Assim, você mantém uma rotina de estudo organizada</p>
-                        </div>
-
-                        <figure id="imgPrint">
-                        <img src="/image/telaPlanoEstudo.png" alt=""/>
-                        </figure>
-
-                    </div>
-
-                </section>
-
-                <section id="depoimentos">
-
-                    <div className="container">
-
-                        <h2 id="tituloDepoimento">Depoimentos</h2>
-
-                        <section id="containerDepoimentos">
-
-                            <article className="boxDepoimento">
-
-                                <h3 className="aspas">“...Passei na federal”</h3>
-
-                                <div className="boxDoComentario">
-                                    <p className="comentario">
-                                        " O Foca foi essencial para mim. A plataforma facilitou muito meus estudos, pois não fazia cursinho e estudava e organizava o que estudaria por conta própria. ”
-
-                                    </p>
-                                </div>
-
-                                <div className="perfilEnome">
-                                    <figure className="imgPerfilDepoimento">
-                                        <img src="/image/avatar.png" alt=""/>
-                                    </figure>
-
-                                    <p className="nomeEstudante">Felipe Nunes</p>
-                                </div>
-
-                            </article>
-
-                            <article className="boxDepoimento">
-
-                                <h3 className="aspas">“...consegui uma bolsa no Brasilescola”</h3>
-
-                                <div className="boxDoComentario">
-                                    <p className="comentario">
-                                        " O Foca foi essencial para mim. A plataforma facilitou muito meus estudos, pois não fazia cursinho e estudava e organizava o que estudaria por conta própria. ”
-
-                                    </p>
-                                </div>
-
-                                <div className="perfilEnome">
-                                    <figure className="imgPerfilDepoimento">
-                                        <img src="/image/avatar.png" alt=""/>
-                                    </figure>
-
-                                    <p className="nomeEstudante">Kátia Luna</p>
-                                </div>
-
-                            </article>
-
-                    
-                        </section>
-               
-                    </div>
-                
-                </section>
-            
+                 
             </main>
             
         </>
